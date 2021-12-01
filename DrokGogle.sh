334a0b3fe44abcfad43046706db2a45d4367894e
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-12-01 11:50:55 +0700

    Create DrokGogle.sh

---
+++ DrokGogle.sh
@@ -0,0 +1,151 @@
+# Banget Dahh
+# [---Author : T1T4N---]
+# Waitting....
+
+# ucapan
+clear
+figlet "Selamat"|lolcat
+figlet "Datang"|lolcat
+
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+yellowlight='\033[1;33m'
+blue='\033[36;1m'
+bluelight='\033[0;36m'
+# menu
+echo -e $red"Harap Masukan Password Yg Di Bawah"
+echo -e $red"Dan Jangan Lupa, Subscribe"$green "T1T4N"
+echo -e $blue"##############################"
+echo -e $blue"### https://bit.ly/31mPlcg ###"
+echo -e $blue"##############################"
+echo
+     echo -e $green "Masukan Passwordnya, Ngabb"
+     read -p "Pass =>" pass
+if [[ $pass == "MBTDork" || $pass == "MBTDork" ]]
+then
+echo -e $yellow "###############"
+echo -e $yellow "# SUCCEESSSSS #"
+echo -e $yellow "###############"
+else
+sleep 0.1
+echo -e $red "#############"
+echo -e $red "# Faiileddd #"
+echo -e $red "#############"
+sleep 2
+bash DrokGogle.sh
+sleep 3
+fi
+clear
+echo -e $black "#########################"
+echo -e $red   "Author : Mr.T1T4N"
+echo -e $red   "Team : ITL | MBT | LCT | CDD"
+echo -e $red   "Dear Mastah : Ajarin Yang Newbie"
+echo -e $black "#########################"
+echo
+echo -e $yellow "HALO FAREL HAXOR"
+echo -e $yellow "LU ABIS NGEWE?"
+echo -e $yellow "LU MAU PAKE DORK INI YA RELL?"
+echo
+echo -e $black "########################"
+echo -e $green "1]. Dork SqlMap"
+echo -e $green "2]. Dork Games Random"
+echo -e $green "3]. Dork Bypass"
+echo -e $green "4]. Dork RFM"
+echo -e $green "5]. Dork Berita"
+echo -e $green "6]. Dork Web Negeri"
+echo -e $green "7]. Dork Site"
+echo -e $green "8]. Tebas Shell Orang"
+echo -e $green "00]. Keluar Lu"
+echo -e $black "#######################"
+    read -p "Masukan Aja :" who
+if [[ $who == 1 || $who == 1 ]]
+then
+clear
+echo -e $red "Tunggu Ngabb"
+     sleep 5
+      echo -e $yellowlight "inurl:news.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:download.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:cat.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:berita.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:index.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:read.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:preview.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:theme.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:pages.php?id="
+     sleep 5
+       echo -e $yellowlight "inurl:main.php?type="
+     sleep 5
+       echo -e $yellowlight "inurl:enter.php?name="
+     sleep 5
+       echo -e $yellowlight "inurl:home.php?sp="
+     sleep 5
+       echo -e $yellowlight "inurl:index.php?to="
+     sleep 5
+       echo -e $yellowlight "inurl:page.php?cmd="
+     sleep 5
+       echo -e $yellowlight "inurl:show.php?x="
+fi
+if [[ $who == 2 || $who == 2 ]]
+then
+clear
+echo -e $red "TUNGGU NGAB"
+     sleep 5
+        echo -e $yellowlight "inurl:store.asinx?game_id="
+     sleep 5
+        echo -e $yellowlight "inurl:BATTLEPASS.htm?type="
+     sleep 5
+        echo -e $yellowlight "inurl:company.htm?type="
+     sleep 5
+        echo -e $yellowlight "inurl:wraps.flv?page_id="
+     sleep 5
+        echo -e $yellowlight "inurl:BATTLEPASS.tss?type="
+     sleep 5
+        echo -e $yellowlight "inurl:store.tss?type_id="
+     sleep 5
+        echo -e $yellowlight "inurl:store.tss?cat="
+     sleep 5
+        echo -e $yellowlight "inurl:store.tss?store_id="
+     sleep 5
+        echo -e $yellowlight "inurl:help.aspx?avd="
+     sleep 5
+        echo -e $yellowlight "inurl:careers.jsf?id="
+     sleep 5
+        echo -e $yellowlight "aw.asinx?sec="
+     sleep 5
+        echo -e $yellowlight "inurl:freegg.com.jsp?include="
+     sleep 5
+        echo -e $yellowlight "inurl:gaming legends.php3?purchase_id="
+     sleep 5
+        echo -e $yellowlight "inurl:free skins.php3?include="
+     sleep 5
+        echo -e $yellowlight "inurl:fortnite friends.aspx?avd="
+     sleep 5
+        echo -e $yellowlight "inurl:fortnite ebay.tss?page_id="
+     sleep 5
+        echo -e $yellowlight "inurl:gaming arcade.psml?cat="
+     sleep 5
+        echo -e $yellowlight "inurl:careers.asp?type=
+fi
+if [[ $who == 3 || $who == 3 ]]
+then
+clear
+echo -e $red "TUNGGU NGAB"
+     sleep 5
+        echo -e $yellowlight "inurl:/index.php/login.phpid?"
+     sleep 5
+        echo -e $yellowlight "inurl:login.php?id="
+     sleep 5
+        echo -e $green "TUNGGU BESOK LAGI"
+fi
